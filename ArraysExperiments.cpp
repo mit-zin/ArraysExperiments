ee002657167cb4cd7f57405d0a3b9dd1e1e2fb41
Dmitriy Zinovyev <mit2005zin@yandex.ru>
2024-08-27 13:29:04 +0300

    Добавил ошибки

--- ArraysExperiments.cpp
+++ ArraysExperiments.cpp
@@ -2,6 +2,8 @@
 #include <stdio.h>
 
 void Print1dAr(int * ar, size_t arSize);
+void Er1Print1dAr(int * ar, size_t arSize);
+void Er2Print1dAr(int * ar, size_t arSize);
 
 int main(void)
 {
@@ -10,6 +12,8 @@ int main(void)
     size_t ar1Size = sizeof(ar1) / sizeof(ar1[0]);
 
     Print1dAr(ar1, ar1Size);
+    Er1Print1dAr(ar1, ar1Size);
+    Er2Print1dAr(ar1, ar1Size);
 }
 
 void Print1dAr(int *ar, size_t arSize)
@@ -17,9 +21,28 @@ void Print1dAr(int *ar, size_t arSize)
     for (size_t x = 0; x < arSize; x++)
     {
         assert(x < arSize);
+        assert(x >= 0);
 
         printf("%8d", *(ar + x));
     }
 
     printf("\n\n");
 }
+
+void Er1Print1dAr(int *ar, size_t arSize)
+{
+    for (size_t x = 0; x < arSize; x++)
+        printf("%8d", *(ar - x));
+
+    printf("\n\n");
+}
+
+void Er2Print1dAr(int * ar, size_t arSize)
+{
+    for (size_t x = 0; x < arSize; x++)
+        printf("%8d", ar + x);
+
+    printf("\n\n");
+}
+
+
